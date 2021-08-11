c98d1ad8a70e5de1be3fd8a815c00ccd357253be
Aniket Pandharabale <aniketicloud@gmail.com>
2021-08-11 21:23:38 +0530

    first implement of graphQL

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby';
 import * as React from 'react';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>About Page!</h1>
+      <Link to="/">Navigate to main page</Link>
     </main>
   );
 }
